b6c7bafa04ea40537fc3d07359586a9a354b809a
chikchicly <chikchicly@gmail.com>
2023-04-03 19:47:09 +0700

    news comments

--- server.js
+++ server.js
@@ -24,16 +24,65 @@
 //   console.log('request');
 //   response.end('go');
 // }),listen(3003);
-const http = require('http');
-const url=require('url');
-const port=3001;
-http.createServer((request, response)=>{
-  console.log(request);
-    response.end('Hello NodeJS!');
-    let urlRequest = url.parse(request.url,true);
-    console.log(urlRequest.query);
-  }).listen(port, '127.0.0.1',()=> {
-    console.log(
-      'Сервер начал прослушивание запросов на порту '+ `${port}`
-    )
-  })
+
+
+const express = require('express');
+const app = express();
+// создаем парсер для данных в формате json
+const jsonParser = express.json();
+
+app.post('/', jsonParser, function (request, response) {
+  // console.log(request.body);
+  if (!request.body) return response.sendStatus(400)
+  response.json(request.body); // отправляем пришедший ответ обратно
+})
+// app.get('/', function (request, response) {
+//   response.sendFile(__dirname + '/comments.html')
+// })
+app.listen(3000);
+
+// const http = require('http');
+// const url=require('url');
+// const { parse }=require('querystring');
+// const port=3000;
+// http.createServer((request, response)=>{
+//   if(request.method=='GET'){
+//     console.log(request);
+//     response.end('Hello NodeJS!'+'_'+request.url+'^_^');
+//     let urlRequest = url.parse(request.url,true);
+//     console.log(urlRequest.query);
+//     console.log(request.method);
+//   }else if(request.method=='POST'){
+//     console.log("POST ^_^");
+//     console.log(request.method);
+//     response.end("Hello method post!");
+//     let body='';
+//     request.on('data',chunk=>{
+//       body+=chunk.toString();
+//       console.log(body.key);
+//       let params = parse(body);
+//       console.log(params);
+//       console.log(params.message);
+//     });
+//   }else{
+//     request.on('end',()=>{
+//       console.log(body);
+//       let params = parse(body);
+//       console.log(params);
+//       console.log(params.message);
+//       response.end('ok');
+//     });
+//     console.log('ELSE '+request.method);
+//     request.on('end',()=>{
+//       console.log(body);
+//       let params = parse(body);
+//       console.log(params);
+//       console.log(params.message);
+//       response.end('ok');
+//     });
+//   }
+// }).listen(port, '127.0.0.1',()=> {
+//     console.log(
+//       'Сервер начал прослушивание запросов на порту '+ `${port}`
+//     )
+//   })
