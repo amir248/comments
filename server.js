8c1aa23df1b871cbe801ef8c97bedee5ef2ed0ab
chikchicly <chikchicly@gmail.com>
2023-04-05 11:16:49 +0700

    oJ

--- server.js
+++ server.js
@@ -28,18 +28,69 @@
 
 const express = require('express');
 const app = express();
+const fs =require('fs');
+
+// const cors = require("cors");
+// const {createProxyMiddleware}=require('http-proxy-middleware');
+// app.use(cors());
+//
+// const API_SERVICE_URL='https:nasobe.ru';
+// process.env["NODE_TLS_REJECT_UNAUTHORIZED"]=0;
+//
+// app.use(cors({
+//   exponsedHeaders: '*'
+// }));
+// app.use("/", createProxyMiddleware({
+//   target: API_SERVICE_URL,
+//   changeOrigin: true,
+//   ws: true,
+//   logLevel: "debug"
+// }));
+
+
+app.use(express.static('/public'));
+app.use('/public', express.static(__dirname + 'json'));
+console.log(__dirname);
+// var cors = require('cors')
+//
+// app.use(cors())
+//
+// app.get('/comments/json', function (req, res, next) {
+//   res.json({msg: Access-Control-Allow-Origin https://nasobe.ru})
+// })
+
+// app.listen(80, function () {
+//   console.log('CORS-enabled web server listening on port 80')
+// })
+
+
 // создаем парсер для данных в формате json
 const jsonParser = express.json();
 const port=3000;
-app.post('/', jsonParser, function (request, response) {
+const host = 'localhost';
+
+app.post('/comments', jsonParser, function (request, response) {
   // console.log(request.body);
-  if (!request.body) return response.sendStatus(400)
+  if (!request.body) return response.sendStatus(400);
+  console.log(request.body);
+  let oki=JSON.stringify(request.body);
+  fs.stat('public/json/message.html',(err,stats)=>{
+    if(err){
+      console.log('fine non');
+      fs.writeFileSync('public/json/message.html',`${oki}`);
+    }else if(stats){
+      console.log('file EST');
+      fs.appendFileSync('public/json/message.html',`${oki}`);
+    }else{
+      console.log('ELSE')
+    }
+  })
   response.json(request.body); // отправляем пришедший ответ обратно
 })
-app.get('/', function (request, response) {
+app.get('/comments', function (request, response) {
   response.sendFile(__dirname + '/comments.html')
 })
-app.listen(port, '127.0.0.1',()=> {
+app.listen(port, host,()=> {
     console.log(
       'Сервер начал прослушивание запросов на порту '+ `${port}`
     )
