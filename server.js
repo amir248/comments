54d11a122cec4e2aaf0ccf5fa1acee766514d3b1
chikchicly <chikchicly@gmail.com>
2023-04-06 11:57:32 +0700

    ejs

--- server.js
+++ server.js
@@ -60,8 +60,15 @@ app.post('/comments/', jsonParser, function (request, response) {
   });
   response.json(request.body); // отправляем пришедший ответ обратно
 });
-app.get('/comments', function (request, response) {
-  response.sendFile(__dirname + '/comments.html')
+app.set("view engine", "ejs");
+// app.get('/comments', function (request, response) {
+//   response.sendFile(__dirname + '/comments.html')
+// });
+app.use("/comments",(request,response)=>{
+  response.render("comments",{
+    title:"Comments",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  });
 });
 app.listen(port, host,()=> {
     console.log(
